ea950a97fcb3aacf39bb78c60230e5b104cbe437
Juan Cavalcante <juanvictor_bc@hotmail.com>
2025-01-16 18:32:41 -0300

    project created

---
+++ src/main/java/com/lptec/section22/Section22Application.java
@@ -0,0 +1,13 @@
+package com.lptec.section22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section22Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lptec/section22/Section22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lptec.section22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
