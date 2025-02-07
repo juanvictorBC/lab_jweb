8fea48ba33568cc81d2e41006741f4fd101f0993
Juan Cavalcante <juanvictor_bc@hotmail.com>
2025-02-06 22:01:02 -0300

    Payment, one-to-one association

--- src/main/java/com/lptec/section22/config/TestConfig.java
+++ src/main/java/com/lptec/section22/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.lptec.section22.entities.Category;
 import com.lptec.section22.entities.Order;
 import com.lptec.section22.entities.OrderItem;
+import com.lptec.section22.entities.Payment;
 import com.lptec.section22.entities.Product;
 import com.lptec.section22.entities.User;
 import com.lptec.section22.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 		
 		
