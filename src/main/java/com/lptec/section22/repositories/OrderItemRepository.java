d3e42be84894b239e6bded50c325617879116beb
Juan <juanvictor_bc@hotmail.com>
2025-09-28 11:54:24 -0300

    correção na OrderItemRepository

--- src/main/java/com/lptec/section22/repositories/OrderItemRepository.java
+++ src/main/java/com/lptec/section22/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.lptec.section22.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lptec.section22.entities.OrderItem;
+import com.lptec.section22.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 
 }
